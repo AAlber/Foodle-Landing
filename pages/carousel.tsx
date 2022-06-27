a3610ce23e6dee8aa0be45c43005a4f34a7e06c7
Alex <alex.alber.aa@gmail.com>
2022-06-27 16:10:29 +0200

    fix: Removed old Carousel

--- pages/carousel.tsx
+++
@@ -1,6 +0,0 @@
-import { NextPage } from 'next';
-import Carousel from '../../foodle/components/Layout/Carousel';
-const CarouselPage: NextPage = () => {
-  return <Carousel />;
-};
-export default CarouselPage;
