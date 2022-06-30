439ed0c9279dc08c1fe7a2798b846557c9cf4792
conuko <constantinu93@gmail.com>
2022-06-30 16:57:07 +0200

    feat: add correct content from navbar to the sidebar

--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -1,36 +1,32 @@
 import Link from 'next/link';
 import { slide as Menu } from 'react-burger-menu';
+import { FormattedMessage, useIntl } from 'react-intl';
+import Tab from './Tab';
 
 const Sidebar = () => {
+  const intl = useIntl();
+  const findKitchen = intl.formatMessage({ id: 'component.navbar.find' });
+  const listKitchen = intl.formatMessage({ id: 'component.navbar.list' });
+  const contact = intl.formatMessage({ id: 'component.navbar.contact' });
+
   const FoodleHamburger = () => (
-    <svg width="31" height="21" viewBox="0 0 31 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect y="0.5" width="16" height="2" fill="black" />
-      <rect y="9.5" width="31" height="2" fill="black" />
-      <rect x="16" y="18.5" width="15" height="2" fill="black" />
-    </svg>
-  );
-  return (
-    <div className="right">
-      <div id="outer-container">
-        <Menu right customBurgerIcon={<FoodleHamburger />} width={300}>
-          <Link href="/">
-            <a className="menu-item">DE</a>
-          </Link>
-          <Link href="/">
-            <a className="menu-item">F.A.Q.</a>
-          </Link>
-          <Link href="/">
-            <a className="menu-item">Unsere Küchen</a>
-          </Link>
-          <Link href="/">
-            <a className="menu-item">Vermiete deine Küche</a>
-          </Link>
-          <Link href="/">
-            <a className="menu-item">Kontakt</a>
-          </Link>
-        </Menu>
-      </div>
+    <div className="svg-burger">
+      <svg width="31" height="21" viewBox="0 0 31 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect y="0.5" width="16" height="2" fill="black" />
+        <rect y="9.5" width="31" height="2" fill="black" />
+        <rect x="16" y="18.5" width="15" height="2" fill="black" />
+      </svg>
     </div>
+  );
+
+  return (
+    <Menu right customBurgerIcon={<FoodleHamburger />} width={300}>
+      <Tab href="/" iconSrc="./world-icon.svg" title="EN" />
+      <Tab href="/" title="F.A.Q." />
+      <Tab href="/" title={findKitchen} />
+      <Tab href="/" title={listKitchen} />
+      <Tab href="/" title={contact} />
+    </Menu>
   );
 };
 
