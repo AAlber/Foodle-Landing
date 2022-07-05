e48a8479f67fe997de2a75d427fb4632f042050f
Alex <alex.alber.aa@gmail.com>
2022-07-06 01:15:34 +0200

    feat: Add minor changes to burger menu

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
         {/* </div> */}
       </div>
       <div className={styles['navbar__menu']}>
-        <Tab href="/" iconSrc="./world-icon.svg" title="EN" />
+        <Tab href="/" iconSrc="./world-icon.svg" title="EN" burger={false} />
         <Tab href="/" title="F.A.Q." />
         <Tab href="/" title={findKitchen} />
         <Tab href="/" title={listKitchen} />
--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -2,6 +2,9 @@ import Link from 'next/link';
 import { slide as Menu } from 'react-burger-menu';
 import { FormattedMessage, useIntl } from 'react-intl';
 import Tab from './Tab';
+import Image from 'next/image';
+
+import styles from './Footer.module.scss';
 
 const Sidebar = () => {
   const intl = useIntl();
@@ -21,11 +24,46 @@ const Sidebar = () => {
 
   return (
     <Menu right customBurgerIcon={<FoodleHamburger />} width={300}>
-      <Tab href="/" iconSrc="./world-icon.svg" title="EN" />
-      <Tab href="/" title="F.A.Q." />
-      <Tab href="/" title={findKitchen} />
-      <Tab href="/" title={listKitchen} />
-      <Tab href="/" title={contact} />
+      <header className="bm-top">
+        <div className="bm-top__right">
+          <Tab href="/" iconSrc="./world-icon.svg" title="EN" />
+        </div>
+        <div className="bm-top__left mt-two">
+          <Tab href="/" title={findKitchen} burger />
+          <Tab href="/" title={listKitchen} burger />
+          <Tab href="/" title="F.A.Q." burger />
+          <Tab href="/" title={contact} burger />
+        </div>
+      </header>
+      <footer>
+        <div className="flex-center">
+          <Image src={'/foodle_logo.svg'} alt="foodle logo" width={40} height={29} />
+          <h3 className={styles['footer__logo'] + ' logo-text'}>Foodle</h3>
+        </div>
+        <section className={styles['footer__end--socials'] + ' flex-center'}>
+          <Image
+            className={styles['footer__end--socials__image']}
+            src={'/instagram.svg'}
+            alt="instagram logo"
+            width={30}
+            height={30}
+          />
+          <Image
+            className={styles['footer__end--socials__image']}
+            src={'/twitter.svg'}
+            alt="twitter logo"
+            width={30}
+            height={30}
+          />
+          <Image
+            className={styles['footer__end--socials__image']}
+            src={'/youtube.svg'}
+            alt="youtube logo"
+            width={30}
+            height={30}
+          />
+        </section>
+      </footer>
     </Menu>
   );
 };
