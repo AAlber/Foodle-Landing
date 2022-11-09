b810b2354fdcdbd4f41b9d67253a84a61a5050f7
Alex Alber <alex.alber.aa@gmail.com>
2022-11-09 15:55:09 +0100

    feat: Add Changes to trustNumbers

--- components/landing/TrustNumbers/index.tsx
+++ components/landing/TrustNumbers/index.tsx
@@ -10,13 +10,13 @@ function TrustNumbers() {
   const trustNumbers = [trustNumber1,trustNumber2,trustNumber3]
   return (
     <div className="flex-center">
-            <div className={styles['trustNumbers']}>
+            <div className={styles['stats']}>
               {['0€', '+15', '0'].map((number, index) => {
                 return (
-                  <div key= {index} className={styles['trustNumbers__container'+(index===2? "--last-child":"")]}>
-                    <p className={styles['trustNumbers__number']}>{number}</p>
+                  <div key= {index} className={styles['stat']}>
+                    <p className={styles['stat__amount']}>{number}</p>
                     <div className="flex-center">
-                      <p className={styles['trustNumbers__text'+(index===2? "--last-child":"")] + ' body-text'}>{trustNumbers[index]}</p>
+                      <p className={styles['stat__text'] + ' body-text'}>{trustNumbers[index]}</p>
                     </div>
                   </div>
                 );
