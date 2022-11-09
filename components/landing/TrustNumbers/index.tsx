b30a3a455a9b0a7128bf4a7ef35a470908441eea
Alex Alber <alex.alber.aa@gmail.com>
2022-11-09 15:35:09 +0100

    feat: Improve seo

--- components/landing/TrustNumbers/index.tsx
+++ components/landing/TrustNumbers/index.tsx
@@ -7,16 +7,16 @@ function TrustNumbers() {
   const trustNumber1 = intl.formatMessage({ id: 'page.home.hero.trustNumber.1' });
   const trustNumber2 = intl.formatMessage({ id: 'page.home.hero.trustNumber.2' });
   const trustNumber3 = intl.formatMessage({ id: 'page.home.hero.trustNumber.3' });
+  const trustNumbers = [trustNumber1,trustNumber2,trustNumber3]
   return (
     <div className="flex-center">
             <div className={styles['trustNumbers']}>
               {['0€', '+15', '0'].map((number, index) => {
-                let trustNumber = [trustNumber1, trustNumber2, trustNumber3][index];
                 return (
                   <div key= {index} className={styles['trustNumbers__container'+(index===2? "--last-child":"")]}>
                     <p className={styles['trustNumbers__number']}>{number}</p>
                     <div className="flex-center">
-                      <p className={styles['trustNumbers__text'+(index===2? "--last-child":"")] + ' body-text'}>{trustNumber}</p>
+                      <p className={styles['trustNumbers__text'+(index===2? "--last-child":"")] + ' body-text'}>{trustNumbers[index]}</p>
                     </div>
                   </div>
                 );
